8f1233b30e798dff2ca52e2bb6579d1d44504f8e
Bhargav Naik <98703553+Bhargav9989sk@users.noreply.github.com>
2026-08-21 20:28:03 +0530

    Update battery_health_prediction_deployment.py

--- battery_health_prediction_deployment.py
+++ battery_health_prediction_deployment.py
@@ -11,7 +11,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-model = joblib.load("Battery_Health_prediction_model.pkl")
+model = joblib.load("Battery_model.pkl")
 encoder = joblib.load("label_encoder.pkl")
 st.title("Battery Health Prediction")
 
